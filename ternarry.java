7ae2168972b5ee61464fef1039c03972427ad819
ghanshyamcode <ghanshyamv2kv@gmial.com>
2021-02-03 20:05:01 +0530

    prg to learn use of ? condition

---
+++ ternarry.java
@@ -0,0 +1,16 @@
+public class ternarry{
+    public static void main(String args[]){
+        int i,k;
+        i=10;
+        k=i<0 ?-i:i;
+        System.out.print("Absolute value of ");
+        System.out.println(i+" is "+k);
+
+        i=-10;
+        k=i<0 ? -i:i;
+        System.out.print("Absolute value of ");
+        System.out.println(i+" is "+k);
+    }
+
+
+}
